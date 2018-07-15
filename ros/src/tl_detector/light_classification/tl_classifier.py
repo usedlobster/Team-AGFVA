ffc70bc4fc4d6b375bc4424fc08e52dc92a435b3
Pascal Irminger <pascal.irminger@gmail.com>
2018-07-15 20:24:58 +0200

    Traffic light classifier (initial)

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,19 +1,58 @@
+import tensorflow as tf
+import numpy as np
 from styx_msgs.msg import TrafficLight
 
 class TLClassifier(object):
     def __init__(self):
-        #TODO load classifier
-        pass
+        self.threshold = .6
+
+        PATH_TO_MODEL = '../../../training_folder/fine_tuned_model/frozen_inference_graph.pb'
+        self.detection_graph = tf.Graph()
+        with self.detection_graph.as_default():
+            od_graph_def = tf.GraphDef()
+            # Works up to here.
+            with tf.gfile.GFile(PATH_TO_MODEL, 'rb') as fid:
+                serialized_graph = fid.read()
+                od_graph_def.ParseFromString(serialized_graph)
+                tf.import_graph_def(od_graph_def, name='')
+            self.image_tensor = self.detection_graph.get_tensor_by_name('image_tensor:0')
+            self.d_boxes = self.detection_graph.get_tensor_by_name('detection_boxes:0')
+            self.d_scores = self.detection_graph.get_tensor_by_name('detection_scores:0')
+            self.d_classes = self.detection_graph.get_tensor_by_name('detection_classes:0')
+            self.num_d = self.detection_graph.get_tensor_by_name('num_detections:0')
+        self.sess = tf.Session(graph=self.detection_graph)
+
+        # TODO: This is just a guess. Values have to be reviewed after training
+        self.tl_mapping = {
+            0 : TrafficLight.RED,
+            1 : TrafficLight.YELLOW,
+            2 : TrafficLight.GREEN
+        }
 
     def get_classification(self, image):
         """Determines the color of the traffic light in the image
-
         Args:
             image (cv::Mat): image containing the traffic light
-
         Returns:
             int: ID of traffic light color (specified in styx_msgs/TrafficLight)
-
         """
-        #TODO implement light color prediction
+        # Bounding Box Detection.
+        with self.detection_graph.as_default():
+            # Expand dimension since the model expects image to have shape [1, None, None, 3].
+            img_expanded = np.expand_dims(image, axis=0)  
+            (boxes, scores, classes, num) = self.sess.run(
+                [self.d_boxes, self.d_scores, self.d_classes, self.num_d],
+                feed_dict={self.image_tensor: img_expanded})
+        #return boxes, scores, classes, num
+        
+        if num == 0:
+            # No bounding boxes found
+            return TrafficLight.UNKNOWN
+        
+        max_score = scores[0]
+        prediction = classes[0]
+
+        if max_score > self.threshold and prediction >= 0 and prediction <= 2:
+            return self.tl_mapping[prediction]
+        
         return TrafficLight.UNKNOWN
