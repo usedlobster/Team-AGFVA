4aa833cf0e96d6932bdb645150e28ceee3b5fb4d
Markus Mainberger <m@mainberger.org>
2018-07-21 21:38:23 +0200

    Retrained model with tf 1.5

--- training_folder/train_real_200.sh
+++ training_folder/train_real_200.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-rm -r ./models/train_real_200
-python3.6 train.py --logtostderr --train_dir=./models/train_real_200 --pipeline_config_path=ssdlite_mobilenet_v2_coco_real_200.config
+rm -rf ./models/train_real_200
+python train.py --logtostderr --train_dir=./models/train_real_200 --pipeline_config_path=ssdlite_mobilenet_v2_coco_real_200.config
 
-rm -r ./fine_tuned_model_real_200
-python3.6 export_inference_graph.py --input_type image_tensor --pipeline_config_path ./ssdlite_mobilenet_v2_coco_real_200.config --trained_checkpoint_prefix ./models/train_real_200/model.ckpt-200 --output_directory ./fine_tuned_model_real_200
+rm -rf ./fine_tuned_model_real_200
+python export_inference_graph.py --input_type image_tensor --pipeline_config_path ./ssdlite_mobilenet_v2_coco_real_200.config --trained_checkpoint_prefix ./models/train_real_200/model.ckpt-200 --output_directory ./fine_tuned_model_real_200
--- training_folder/train_sim_200.sh
+++ training_folder/train_sim_200.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-rm -r ./models/train_sim_200
-python3.6 train.py --logtostderr --train_dir=./models/train_sim_200 --pipeline_config_path=ssdlite_mobilenet_v2_coco_sim_200.config
+rm -rf ./models/train_sim_200
+python train.py --logtostderr --train_dir=./models/train_sim_200 --pipeline_config_path=ssdlite_mobilenet_v2_coco_sim_200.config
 
-rm -r ./fine_tuned_model_sim_200
-python3.6 export_inference_graph.py --input_type image_tensor --pipeline_config_path ./ssdlite_mobilenet_v2_coco_sim_200.config --trained_checkpoint_prefix ./models/train_sim_200/model.ckpt-200 --output_directory ./fine_tuned_model_sim_200
+rm -rf ./fine_tuned_model_sim_200
+python export_inference_graph.py --input_type image_tensor --pipeline_config_path ./ssdlite_mobilenet_v2_coco_sim_200.config --trained_checkpoint_prefix ./models/train_sim_200/model.ckpt-200 --output_directory ./fine_tuned_model_sim_200
