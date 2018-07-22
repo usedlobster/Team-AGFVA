2cea21894fb9690a0b11430083dc44eac9812b99
Markus Mainberger <m@mainberger.org>
2018-07-22 15:59:30 +0200

    Adapted script files for other model

--- training_folder/train_sim_200.sh
+++ training_folder/train_sim_200.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-rm -rf ./models/train_sim_200
-python train.py --logtostderr --train_dir=./models/train_sim_200 --pipeline_config_path=ssdlite_mobilenet_v2_coco_sim_200.config
+ITER="200"
+PREFIX="real"
+TRAIN_DIR="./models/train_$PREFIX_$ITER"
+CONFIG_PATH="ssd_mobilenet_v1_coco_sim$PREFIX_$ITER.config"
+MODEL_OUT="./fine_tuned_model_sim$PREFIX_$ITER"
 
-rm -rf ./fine_tuned_model_sim_200
-python export_inference_graph.py --input_type image_tensor --pipeline_config_path ./ssdlite_mobilenet_v2_coco_sim_200.config --trained_checkpoint_prefix ./models/train_sim_200/model.ckpt-200 --output_directory ./fine_tuned_model_sim_200
+rm -rf $TRAIN_DIR
+python train.py --logtostderr --train_dir=$TRAIN_DIR --pipeline_config_path=$CONFIG_PATH
+rm -rf $MODEL_OUT
+python export_inference_graph.py --input_type image_tensor --pipeline_config_path $CONFIG_PATH --trained_checkpoint_prefix $TRAIN_DIR/model.ckpt-200 --output_directory $MODEL_OUT
