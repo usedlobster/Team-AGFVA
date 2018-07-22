75c48c2a28529a068622b21795525d0178775284
Markus Mainberger <m@mainberger.org>
2018-07-22 19:21:21 +0200

    Fixes in the training scripts

--- training_folder/train_real_200.sh
+++ training_folder/train_real_200.sh
@@ -2,11 +2,11 @@
 
 ITER="200"
 PREFIX="real"
-TRAIN_DIR="./models/train_$PREFIX_$ITER"
-CONFIG_PATH="ssd_mobilenet_v1_coco_sim$PREFIX_$ITER.config"
-MODEL_OUT="./fine_tuned_model_sim$PREFIX_$ITER"
+TRAIN_DIR="./models/train_${PREFIX}_${ITER}"
+CONFIG_PATH="ssd_mobilenet_v1_coco_${PREFIX}_${ITER}.config"
+MODEL_OUT="./fine_tuned_model_${PREFIX}_${ITER}"
 
 rm -rf $TRAIN_DIR
-python train.py --logtostderr --train_dir=$TRAIN_DIR --pipeline_config_path=$CONFIG_PATH
+echo "python train.py --logtostderr --train_dir=$TRAIN_DIR --pipeline_config_path=$CONFIG_PATH"
 rm -rf $MODEL_OUT
 python export_inference_graph.py --input_type image_tensor --pipeline_config_path $CONFIG_PATH --trained_checkpoint_prefix $TRAIN_DIR/model.ckpt-200 --output_directory $MODEL_OUT
--- training_folder/train_sim_200.sh
+++ training_folder/train_sim_200.sh
@@ -8,5 +8,5 @@ MODEL_OUT="./fine_tuned_model_${PREFIX}_${ITER}"
 
 rm -rf $TRAIN_DIR
 echo "python train.py --logtostderr --train_dir=$TRAIN_DIR --pipeline_config_path=$CONFIG_PATH"
-#rm -rf $MODEL_OUT
-#python export_inference_graph.py --input_type image_tensor --pipeline_config_path $CONFIG_PATH --trained_checkpoint_prefix $TRAIN_DIR/model.ckpt-200 --output_directory $MODEL_OUT
+rm -rf $MODEL_OUT
+python export_inference_graph.py --input_type image_tensor --pipeline_config_path $CONFIG_PATH --trained_checkpoint_prefix $TRAIN_DIR/model.ckpt-200 --output_directory $MODEL_OUT
